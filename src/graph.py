1ac3cff8254ce72bcf885f66e552e80e3cd8cd7e
unknown <eduardo-curcino@hotmail.com>
2017-11-15 17:08:57 -0200

    fix travelTime graph

--- src/graph.py
+++ src/graph.py
@@ -78,8 +78,9 @@ class Graph:
             n, dist = util.medium_point(self.graph, node_data)
 
             if n and not self.nodes_connected(node, n['data'][0]) and node_data[5] != n['data'][5]:
-                # media de tempo de caminhada
-                time = dist / 0.00166667
+                # media de tempo de caminhada, 0.00166667 é km/h
+                time = (dist * 3600) / 6000
+
                 # duas arestas direcionais
                 self.graph.add_edge(node_data[0], n['data'][0], weight=dist, travelTime=time)
         
