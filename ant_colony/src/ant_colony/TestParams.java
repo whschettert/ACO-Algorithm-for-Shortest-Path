d00fb6a682d5b2bb23736305fdcd54ae93b9ab1d
unknown <eduardo-curcino@hotmail.com>
2017-11-15 19:23:35 -0200

    test params

--- ant_colony/src/ant_colony/TestParams.java
+++ ant_colony/src/ant_colony/TestParams.java
@@ -23,16 +23,17 @@ public class TestParams {
 		ArrayList<Tuple<Tuple<Integer, Integer>, Params>> dists_nodes = new ArrayList<>();
 		
 		dists_nodes.addAll(Arrays.asList(
-				new Tuple<Tuple<Integer, Integer>, Params>(new Tuple<Integer, Integer>(10,14), new Params(11000, 500, 0.00454086890194393, 4.22485815330664, 0.098463467848146, 0.738033854470105, "398S5", "423S4")),
-				new Tuple<Tuple<Integer, Integer>, Params>(new Tuple<Integer, Integer>(20,24), new Params(11000, 500, 0.00057428336323527, 1.40113839186818, 0.671867740355784,	0.932520435189094, "439S1", "525S9")),
-				new Tuple<Tuple<Integer, Integer>, Params>(new Tuple<Integer, Integer>(30,34), new Params(16000, 500, 0.00938973013753504, 4.89074108324041, 0.236287969061914, 0.70157055583896, "591S48", "416S27")),
-				new Tuple<Tuple<Integer, Integer>, Params>(new Tuple<Integer, Integer>(40,44), new Params(17000, 500, 0.00357954034147196, 4.75812603323567, 2.50055407112918, 0.764991688847031, "311S13", "363S19")),
-				new Tuple<Tuple<Integer, Integer>, Params>(new Tuple<Integer, Integer>(50,54), new Params(17000, 500, 0.00601493670647097, 0.42515935150438, 0.0248860232737169, 0.904061270232578, "98S8", "591S63")),
-				new Tuple<Tuple<Integer, Integer>, Params>(new Tuple<Integer, Integer>(60,64), new Params(14000, 500, 0.00379520320924374, 1.81887566191999, 0.0252688911255433, 0.644589642143173, "564S2", "637S19")),
-				new Tuple<Tuple<Integer, Integer>, Params>(new Tuple<Integer, Integer>(70,74), new Params(11000, 500, 0.00173846712644101, 4.66500425419624, 0.204994981834971, 0.893953894928224, "527S8", "591S86")),
-				new Tuple<Tuple<Integer, Integer>, Params>(new Tuple<Integer, Integer>(80,84), new Params(13000, 500, 0.000426261492134647, 4.08933257373996, 0.0964400208487564, 0.688435080629192, "490S8", "637S26")),
-				new Tuple<Tuple<Integer, Integer>, Params>(new Tuple<Integer, Integer>(90,94), new Params(11000, 500, 0.00266975604879154, 1.02266783665256, 0.0417999511611427, 0.358615034407506, "464S17", "637S26")),
-				new Tuple<Tuple<Integer, Integer>, Params>(new Tuple<Integer, Integer>(100,104), new Params(11000, 500, 0.00492663697122965, 2.13472992853583, 0.666989318093701, 0.329770983735447, "604S1", "637S31"))
+				new Tuple<Tuple<Integer, Integer>, Params>(new Tuple<Integer, Integer>(20,24), new Params(10000, 500, 0.001300568018154422, 3.5453538979317614, 0.33500983208537893, 0.37870467710191685, "39S1", "61S0")),
+				new Tuple<Tuple<Integer, Integer>, Params>(new Tuple<Integer, Integer>(10,14), new Params(16000, 500, 0.0063353088584364, 1.3962429970803614, 0.3068086831856759, 0.3099138008830667, "241S1", "160S11")),
+				new Tuple<Tuple<Integer, Integer>, Params>(new Tuple<Integer, Integer>(30,34), new Params(13000, 500, 0.0023336142115786846, 0.522514651109075, 0.0765991524247367, 0.5811955425428774, "458S2", "57S5")),
+				new Tuple<Tuple<Integer, Integer>, Params>(new Tuple<Integer, Integer>(40,44), new Params(14000, 500, 0.0010440373512414299, 1.7263098448602787, 0.06647868855316985, 0.8880839857525336, "174S5", "232S43")),
+				new Tuple<Tuple<Integer, Integer>, Params>(new Tuple<Integer, Integer>(50,54), new Params(17000, 500, 0.006334488679871194, 0.11505926807957145, 0.00744865219720503, 0.42248344822971867, "641S3", "174S10")),
+				new Tuple<Tuple<Integer, Integer>, Params>(new Tuple<Integer, Integer>(60,64), new Params(13000, 500, 0.008191928718105097, 1.767255738432569, 1.8682476134382369, 0.34204419349976317, "42S0", "37S23")),
+				new Tuple<Tuple<Integer, Integer>, Params>(new Tuple<Integer, Integer>(70,74), new Params(14000, 500, 0.00536819648506351, 1.7453084763243891, 0.4125457268129333, 0.37586866066323293, "41S26", "130S15")),				
+				new Tuple<Tuple<Integer, Integer>, Params>(new Tuple<Integer, Integer>(80,84), new Params(17000, 500, 3.097382799551567E-4, 0.6479181999934513, 0.20324537450723934, 0.5238581133892463, "625S9", "130S17")),
+				new Tuple<Tuple<Integer, Integer>, Params>(new Tuple<Integer, Integer>(90,94), new Params(18000, 500, 3.4569712402988365E-4, 2.2792071390392543, 0.7486322909156092, 0.7472330760872458, "253S2", "130S22")),
+				new Tuple<Tuple<Integer, Integer>, Params>(new Tuple<Integer, Integer>(100,104), new Params(13000, 500, 0.002979989166906917, 4.342049917001485, 0.11152733342115806, 0.7565399197229654, "162S8", "130S27"))
+				
 				));
 		
 		String source, target = null;
